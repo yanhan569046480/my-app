c35ced31b9c1ffe06f7cf4b660e8e19b191bdde0
yanhan <yanhan@megaease.com>
2022-08-14 22:14:04 +0800

    .

--- src/test/CanaryUtil.js
+++ src/test/CanaryUtil.js
@@ -1,151 +1,163 @@
-function isCanaryShadow(
-    canaryMatrix,
-    highWeightCanaryMap,
-    lowWeightCanaryMap,
+function isCanaryShadow(canaryMatrix, highWeightCanaryMap, lowWeightCanaryMap) {
+  // console.log('canaryMatrix', canaryMatrix);
+  let supersetCanary = canaryMatrix[0];
+  let subsetCanary = canaryMatrix[1];
+  // console.log('supersetCanary', supersetCanary);
+  // console.log('subsetCanary', subsetCanary);
+
+  if (
+    supersetCanary &&
+    supersetCanary.trafficRules &&
+    supersetCanary.trafficRules.headers &&
+    supersetCanary.trafficRules.headers.length > 0 &&
+    subsetCanary &&
+    subsetCanary.trafficRules &&
+    subsetCanary.trafficRules.headers &&
+    subsetCanary.trafficRules.headers.length > 0
   ) {
-    // console.log('canaryMatrix', canaryMatrix);
-    let supersetCanary = canaryMatrix[0];
-    let subsetCanary = canaryMatrix[1];
-    // console.log('supersetCanary', supersetCanary);
-    // console.log('subsetCanary', subsetCanary);
-  
     if (
-      supersetCanary &&
-      supersetCanary.trafficRules &&
-      supersetCanary.trafficRules.headers &&
-      supersetCanary.trafficRules.headers.length > 0 &&
-      subsetCanary &&
-      subsetCanary.trafficRules &&
-      subsetCanary.trafficRules.headers &&
-      subsetCanary.trafficRules.headers.length > 0
+      supersetCanary.trafficRules.headers.length <
+      subsetCanary.trafficRules.headers.length
     ) {
-      if (
-        supersetCanary.trafficRules.headers.length <
-        subsetCanary.trafficRules.headers.length
-      ) {
-        [supersetCanary, subsetCanary] = [subsetCanary, supersetCanary];
-        // console.log('supersetCanary', supersetCanary);
-        // console.log('subsetCanary', subsetCanary);
-      }
-  
-      let flag = true;
-      subsetCanary.trafficRules.headers.forEach((subHeader) => {
-        let findSuperHeader = supersetCanary.trafficRules.headers.find(
-          (superHeader) => {
-            return superHeader.key == subHeader.key;
-          },
-        );
-        if (!findSuperHeader) {
-        //   console.log('findSuperHeader', findSuperHeader);
-          supersetCanary.style = {
-            stroke: '#FF5C00',
-          };
-          supersetCanary.weight = 'high';
-  
-          subsetCanary.style = {
-            stroke: '#FF5C00',
-          };
-          subsetCanary.weight = 'high';
-  
-          highWeightCanaryMap.set(supersetCanary.name, supersetCanary);
-          highWeightCanaryMap.set(subsetCanary.name, subsetCanary);
-  
-          flag = false;
-        }
-      });
-      if (!flag) {
-        return false;
-      }
-  
-      if (
-        supersetCanary.trafficRules.headers.length ==
-        subsetCanary.trafficRules.headers.length
-      ) {
-        supersetCanary.style = {
-          stroke: '#D8D8D8',
-          lineDash: [5, 5],
-        };
-        supersetCanary.weight = 'low';
-  
-        subsetCanary.style = {
-          stroke: '#D8D8D8',
-          lineDash: [5, 5],
-        };
-        subsetCanary.weight = 'low';
-  
-        lowWeightCanaryMap.set(supersetCanary.name, supersetCanary);
-        lowWeightCanaryMap.set(subsetCanary.name, subsetCanary);
-  
-        return true;
-      }
-  
-      if (supersetCanary.priority < subsetCanary.priority) {
+      [supersetCanary, subsetCanary] = [subsetCanary, supersetCanary];
+      // console.log('supersetCanary', supersetCanary);
+      // console.log('subsetCanary', subsetCanary);
+    }
+
+    let flag = true;
+    subsetCanary.trafficRules.headers.forEach((subHeader) => {
+      let findSuperHeader = supersetCanary.trafficRules.headers.find(
+        (superHeader) => {
+          return superHeader.key == subHeader.key;
+        },
+      );
+      if (!findSuperHeader) {
+        // console.log('findSuperHeader', findSuperHeader);
         supersetCanary.style = {
           stroke: '#FF5C00',
         };
         supersetCanary.weight = 'high';
-  
+
+        subsetCanary.style = {
+          stroke: '#FF5C00',
+        };
+        subsetCanary.weight = 'high';
+
+        highWeightCanaryMap.set(supersetCanary.name, supersetCanary);
+        highWeightCanaryMap.set(subsetCanary.name, subsetCanary);
+
+        flag = false;
+      }
+    });
+    if (!flag) {
+      return false;
+    }
+
+    if (
+      supersetCanary.trafficRules.headers.length ==
+      subsetCanary.trafficRules.headers.length
+    ) {
+      supersetCanary.style = {
+        stroke: '#D8D8D8',
+        lineDash: [5, 5],
+      };
+      supersetCanary.weight = 'low';
+
+      subsetCanary.style = {
+        stroke: '#D8D8D8',
+        lineDash: [5, 5],
+      };
+      subsetCanary.weight = 'low';
+
+      lowWeightCanaryMap.set(supersetCanary.name, supersetCanary);
+      lowWeightCanaryMap.set(subsetCanary.name, subsetCanary);
+
+      return true;
+    }
+
+    if (supersetCanary.priority < subsetCanary.priority) {
+      supersetCanary.style = {
+        stroke: '#FF5C00',
+      };
+      supersetCanary.weight = 'high';
+
+      subsetCanary.style = {
+        stroke: '#D8D8D8',
+        lineDash: [5, 5],
+      };
+      subsetCanary.weight = 'low';
+
+      highWeightCanaryMap.set(supersetCanary.name, supersetCanary);
+      lowWeightCanaryMap.set(subsetCanary.name, subsetCanary);
+
+      return true;
+    } else if (supersetCanary.priority == subsetCanary.priority) {
+      if (supersetCanary.name.localeCompare(subsetCanary.name) == -1) {
+        supersetCanary.style = {
+          stroke: '#FF5C00',
+        };
+        supersetCanary.weight = 'high';
+
         subsetCanary.style = {
           stroke: '#D8D8D8',
           lineDash: [5, 5],
         };
         subsetCanary.weight = 'low';
-  
+
         highWeightCanaryMap.set(supersetCanary.name, supersetCanary);
         lowWeightCanaryMap.set(subsetCanary.name, subsetCanary);
-  
+
         return true;
-      } else if (supersetCanary.priority == subsetCanary.priority) {
-        if (supersetCanary.name.localeCompare(subsetCanary.name) == -1) {
-          supersetCanary.style = {
-            stroke: '#FF5C00',
-          };
-          supersetCanary.weight = 'high';
-  
-          subsetCanary.style = {
-            stroke: '#D8D8D8',
-            lineDash: [5, 5],
-          };
-          subsetCanary.weight = 'low';
-  
-          highWeightCanaryMap.set(supersetCanary.name, supersetCanary);
-          lowWeightCanaryMap.set(subsetCanary.name, subsetCanary);
-  
-          return true;
-        }
-      } else if (supersetCanary.priority > subsetCanary.priority) {
+      }else if (supersetCanary.name.localeCompare(subsetCanary.name) == 1) {
         supersetCanary.style = {
           stroke: '#D8D8D8',
           lineDash: [5, 5],
         };
         supersetCanary.weight = 'low';
-  
+
         subsetCanary.style = {
           stroke: '#FF5C00',
         };
         subsetCanary.weight = 'high';
-  
+
         highWeightCanaryMap.set(subsetCanary.name, subsetCanary);
         lowWeightCanaryMap.set(supersetCanary.name, supersetCanary);
-  
+
         return true;
       }
+    } else if (supersetCanary.priority > subsetCanary.priority) {
+      supersetCanary.style = {
+        stroke: '#D8D8D8',
+        lineDash: [5, 5],
+      };
+      supersetCanary.weight = 'low';
+
+      subsetCanary.style = {
+        stroke: '#FF5C00',
+      };
+      subsetCanary.weight = 'high';
+
+      highWeightCanaryMap.set(subsetCanary.name, subsetCanary);
+      lowWeightCanaryMap.set(supersetCanary.name, supersetCanary);
+
+      return true;
     }
-  
-    supersetCanary.style = {
-      stroke: '#FF5C00',
-    };
-    supersetCanary.weight = 'high';
-  
-    subsetCanary.style = {
-      stroke: '#FF5C00',
-    };
-    subsetCanary.weight = 'high';
-  
-    highWeightCanaryMap.set(supersetCanary.name, supersetCanary);
-    highWeightCanaryMap.set(subsetCanary.name, subsetCanary);
-  
-    return false;
+  }
+
+  supersetCanary.style = {
+    stroke: '#FF5C00',
+  };
+  supersetCanary.weight = 'high';
+
+  subsetCanary.style = {
+    stroke: '#FF5C00',
+  };
+  subsetCanary.weight = 'high';
+
+  highWeightCanaryMap.set(supersetCanary.name, supersetCanary);
+  highWeightCanaryMap.set(subsetCanary.name, subsetCanary);
+
+  return false;
 }
 
 module.exports = isCanaryShadow;
--- src/test/CanaryUtil.test.js
+++ src/test/CanaryUtil.test.js
@@ -1,10 +1,40 @@
 const isCanaryShadow = require('./CanaryUtil');
-const canaryMatrixDifferentSet = require('./canaryMatrixDifferentSet');
+const canaryMatrixDifferentHeaders = require('./canaryMatrixDifferentHeaders');
+const canaryMatrixDifferenceSet = require('./canaryMatrixDifferenceSet');
+const canaryMatrixSameHeaders = require('./canaryMatrixSameHeaders');
+const canaryMatrixDifferentPriority = require('./canaryMatrixDifferentPriority');
+const samePriorityDifferentName = require('./samePriorityDifferentName');
+const samePriorityDifferentName2 = require('./samePriorityDifferentName2');
+const canaryMatrixDifferentPriority2 = require('./canaryMatrixDifferentPriority2');
 
 const highWeightCanaryMap = new Map();
 
 const lowWeightCanaryMap = new Map();
-  
-test('differentSet', () => {
-  expect(isCanaryShadow(canaryMatrixDifferentSet, highWeightCanaryMap, lowWeightCanaryMap)).toBe(true);
+
+test('differentHeaders', () => {
+  expect(isCanaryShadow(canaryMatrixDifferentHeaders, highWeightCanaryMap, lowWeightCanaryMap)).toBe(true);
+});
+
+test('differenceSet', () => {
+  expect(isCanaryShadow(canaryMatrixDifferenceSet, highWeightCanaryMap, lowWeightCanaryMap)).toBe(false);
+});
+
+test('sameHeaders', () => {
+  expect(isCanaryShadow(canaryMatrixSameHeaders, highWeightCanaryMap, lowWeightCanaryMap)).toBe(true);
+});
+
+test('differentPriority', () => {
+  expect(isCanaryShadow(canaryMatrixDifferentPriority, highWeightCanaryMap, lowWeightCanaryMap)).toBe(true);
+});
+
+test('samePriorityDifferentName', () => {
+  expect(isCanaryShadow(samePriorityDifferentName, highWeightCanaryMap, lowWeightCanaryMap)).toBe(true);
+});
+
+test('samePriorityDifferentName2', () => {
+  expect(isCanaryShadow(samePriorityDifferentName2, highWeightCanaryMap, lowWeightCanaryMap)).toBe(true);
+});
+
+test('differentPriority2', () => {
+  expect(isCanaryShadow(canaryMatrixDifferentPriority2, highWeightCanaryMap, lowWeightCanaryMap)).toBe(true);
 });
