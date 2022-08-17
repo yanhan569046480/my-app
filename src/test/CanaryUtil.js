495adfbbf8f1268fc57103d7c3e93c60d82b5483
yanhan <yanhan@megaease.com>
2022-08-17 09:31:30 +0800

    .

--- src/test/CanaryUtil.js
+++ src/test/CanaryUtil.js
@@ -1,163 +1,223 @@
 function isCanaryShadow(canaryMatrix, highWeightCanaryMap, lowWeightCanaryMap) {
   // console.log('canaryMatrix', canaryMatrix);
-  let supersetCanary = canaryMatrix[0];
-  let subsetCanary = canaryMatrix[1];
-  // console.log('supersetCanary', supersetCanary);
-  // console.log('subsetCanary', subsetCanary);
+  let generalSetCanary = canaryMatrix[0];
+  let preciseSetCanary = canaryMatrix[1];
+  // console.log('generalSetCanary', generalSetCanary);
+  // console.log('preciseSetCanary', preciseSetCanary);
 
   if (
-    supersetCanary &&
-    supersetCanary.trafficRules &&
-    supersetCanary.trafficRules.headers &&
-    supersetCanary.trafficRules.headers.length > 0 &&
-    subsetCanary &&
-    subsetCanary.trafficRules &&
-    subsetCanary.trafficRules.headers &&
-    subsetCanary.trafficRules.headers.length > 0
+    generalSetCanary &&
+    generalSetCanary.trafficRules &&
+    generalSetCanary.trafficRules.headers &&
+    generalSetCanary.trafficRules.headers.length > 0 &&
+    preciseSetCanary &&
+    preciseSetCanary.trafficRules &&
+    preciseSetCanary.trafficRules.headers &&
+    preciseSetCanary.trafficRules.headers.length > 0
   ) {
     if (
-      supersetCanary.trafficRules.headers.length <
-      subsetCanary.trafficRules.headers.length
+      generalSetCanary.trafficRules.headers.length >
+      preciseSetCanary.trafficRules.headers.length
     ) {
-      [supersetCanary, subsetCanary] = [subsetCanary, supersetCanary];
-      // console.log('supersetCanary', supersetCanary);
-      // console.log('subsetCanary', subsetCanary);
+      [generalSetCanary, preciseSetCanary] = [
+        preciseSetCanary,
+        generalSetCanary,
+      ];
+      console.log('generalSetCanary', generalSetCanary);
+      console.log('preciseSetCanary', preciseSetCanary);
     }
 
-    let flag = true;
-    subsetCanary.trafficRules.headers.forEach((subHeader) => {
-      let findSuperHeader = supersetCanary.trafficRules.headers.find(
-        (superHeader) => {
-          return superHeader.key == subHeader.key;
+    let exist = true;
+    generalSetCanary.trafficRules.headers.forEach((generalSetHeader) => {
+      let findPreciseHeader = preciseSetCanary.trafficRules.headers.find(
+        (preciseSetHeader) => {
+          return preciseSetHeader.key == generalSetHeader.key;
         },
       );
-      if (!findSuperHeader) {
-        // console.log('findSuperHeader', findSuperHeader);
-        supersetCanary.style = {
-          stroke: '#FF5C00',
-        };
-        supersetCanary.weight = 'high';
-
-        subsetCanary.style = {
-          stroke: '#FF5C00',
-        };
-        subsetCanary.weight = 'high';
-
-        highWeightCanaryMap.set(supersetCanary.name, supersetCanary);
-        highWeightCanaryMap.set(subsetCanary.name, subsetCanary);
-
-        flag = false;
+      if (!findPreciseHeader) {
+        // console.log('findGeneralHeader', findGeneralHeader);
+        exist = false;
       }
     });
-    if (!flag) {
+    if (!exist) {
+      // if(!generalSetCanary.weight){
+      generalSetCanary.style = {
+        stroke: 'l(0) 0:#FF0000 1:#FEA9A9',
+      };
+      generalSetCanary.weight = 'high';
+      generalSetCanary.edgeType = 'custom-edge';
+      highWeightCanaryMap.set(generalSetCanary.name, generalSetCanary);
+      // }
+
+      // if(!preciseSetCanary.weight){
+      preciseSetCanary.style = {
+        stroke: 'l(0) 0:#FF0000 1:#FEA9A9',
+      };
+      preciseSetCanary.weight = 'high';
+      preciseSetCanary.edgeType = 'custom-edge';
+      highWeightCanaryMap.set(preciseSetCanary.name, preciseSetCanary);
+      // }
+
+      console.log('exist', exist);
+
       return false;
     }
 
     if (
-      supersetCanary.trafficRules.headers.length ==
-      subsetCanary.trafficRules.headers.length
+      generalSetCanary.trafficRules.headers.length ==
+      preciseSetCanary.trafficRules.headers.length
     ) {
-      supersetCanary.style = {
-        stroke: '#D8D8D8',
-        lineDash: [5, 5],
+      // if(!generalSetCanary.weight){
+      generalSetCanary.style = {
+        // stroke: '#D8D8D8',
+        // lineDash: [5, 5],
+        stroke: 'l(0) 0:#FF0000 1:#FEA9A9',
       };
-      supersetCanary.weight = 'low';
+      generalSetCanary.weight = 'low';
+      generalSetCanary.edgeType = 'custom-edge';
+      lowWeightCanaryMap.set(generalSetCanary.name, generalSetCanary);
+      // }
 
-      subsetCanary.style = {
-        stroke: '#D8D8D8',
-        lineDash: [5, 5],
+      // if(!preciseSetCanary.weight){
+      preciseSetCanary.style = {
+        // stroke: '#D8D8D8',
+        // lineDash: [5, 5],
+        stroke: 'l(0) 0:#FF0000 1:#FEA9A9',
       };
-      subsetCanary.weight = 'low';
+      preciseSetCanary.weight = 'low';
+      preciseSetCanary.edgeType = 'custom-edge';
+      lowWeightCanaryMap.set(preciseSetCanary.name, preciseSetCanary);
+      // }
 
-      lowWeightCanaryMap.set(supersetCanary.name, supersetCanary);
-      lowWeightCanaryMap.set(subsetCanary.name, subsetCanary);
+      console.log('headers.length.equal', true);
 
       return true;
     }
 
-    if (supersetCanary.priority < subsetCanary.priority) {
-      supersetCanary.style = {
-        stroke: '#FF5C00',
+    if (generalSetCanary.priority < preciseSetCanary.priority) {
+      // if(!generalSetCanary.weight){
+      generalSetCanary.style = {
+        stroke: 'l(0) 0:#FF0000 1:#FEA9A9',
       };
-      supersetCanary.weight = 'high';
+      generalSetCanary.weight = 'high';
+      generalSetCanary.edgeType = 'custom-edge';
+      highWeightCanaryMap.set(generalSetCanary.name, generalSetCanary);
+      // }
 
-      subsetCanary.style = {
+      // if(!preciseSetCanary.weight){
+      preciseSetCanary.style = {
         stroke: '#D8D8D8',
         lineDash: [5, 5],
       };
-      subsetCanary.weight = 'low';
+      preciseSetCanary.weight = 'low';
+      preciseSetCanary.edgeType = 'mid-point-edge';
+      // preciseSetCanary.edgeType = 'custom-edge';
+      lowWeightCanaryMap.set(preciseSetCanary.name, preciseSetCanary);
+      // }
 
-      highWeightCanaryMap.set(supersetCanary.name, supersetCanary);
-      lowWeightCanaryMap.set(subsetCanary.name, subsetCanary);
+      console.log('priority.less.than', true);
 
       return true;
-    } else if (supersetCanary.priority == subsetCanary.priority) {
-      if (supersetCanary.name.localeCompare(subsetCanary.name) == -1) {
-        supersetCanary.style = {
-          stroke: '#FF5C00',
+    } else if (generalSetCanary.priority == preciseSetCanary.priority) {
+      if (generalSetCanary.name.localeCompare(preciseSetCanary.name) == -1) {
+        // if(!generalSetCanary.weight){
+        generalSetCanary.style = {
+          stroke: 'l(0) 0:#FF0000 1:#FEA9A9',
         };
-        supersetCanary.weight = 'high';
+        generalSetCanary.weight = 'high';
+        generalSetCanary.edgeType = 'custom-edge';
+        highWeightCanaryMap.set(generalSetCanary.name, generalSetCanary);
+        // }
 
-        subsetCanary.style = {
+        // if(!preciseSetCanary.weight){
+        preciseSetCanary.style = {
           stroke: '#D8D8D8',
           lineDash: [5, 5],
         };
-        subsetCanary.weight = 'low';
+        preciseSetCanary.weight = 'low';
+        preciseSetCanary.edgeType = 'mid-point-edge';
+        // preciseSetCanary.edgeType = 'custom-edge';
+        lowWeightCanaryMap.set(preciseSetCanary.name, preciseSetCanary);
+        // }
 
-        highWeightCanaryMap.set(supersetCanary.name, supersetCanary);
-        lowWeightCanaryMap.set(subsetCanary.name, subsetCanary);
+        console.log('priority.equal.but.name.less.than', true);
 
         return true;
-      }else if (supersetCanary.name.localeCompare(subsetCanary.name) == 1) {
-        supersetCanary.style = {
+      } else if (
+        generalSetCanary.name.localeCompare(preciseSetCanary.name) == 1
+      ) {
+        // if(!generalSetCanary.weight){
+        generalSetCanary.style = {
           stroke: '#D8D8D8',
           lineDash: [5, 5],
         };
-        supersetCanary.weight = 'low';
+        generalSetCanary.weight = 'low';
+        generalSetCanary.edgeType = 'mid-point-edge';
+        // generalSetCanary.edgeType = 'custom-edge';
+        lowWeightCanaryMap.set(generalSetCanary.name, generalSetCanary);
+        // }
 
-        subsetCanary.style = {
-          stroke: '#FF5C00',
+        // if(!preciseSetCanary.weight){
+        preciseSetCanary.style = {
+          stroke: 'l(0) 0:#FF0000 1:#FEA9A9',
         };
-        subsetCanary.weight = 'high';
+        preciseSetCanary.weight = 'high';
+        preciseSetCanary.edgeType = 'custom-edge';
+        highWeightCanaryMap.set(preciseSetCanary.name, preciseSetCanary);
+        // }
 
-        highWeightCanaryMap.set(subsetCanary.name, subsetCanary);
-        lowWeightCanaryMap.set(supersetCanary.name, supersetCanary);
+        console.log('priority.equal.but.name.more.than', true);
 
         return true;
+      } else {
+        // if(!generalSetCanary.weight){
+        generalSetCanary.style = {
+          stroke: 'l(0) 0:#FF0000 1:#FEA9A9',
+        };
+        generalSetCanary.weight = 'high';
+        generalSetCanary.edgeType = 'custom-edge';
+        highWeightCanaryMap.set(generalSetCanary.name, generalSetCanary);
+        // }
+
+        // if(!preciseSetCanary.weight){
+        preciseSetCanary.style = {
+          stroke: 'l(0) 0:#FF0000 1:#FEA9A9',
+        };
+        preciseSetCanary.weight = 'high';
+        preciseSetCanary.edgeType = 'custom-edge';
+        highWeightCanaryMap.set(preciseSetCanary.name, preciseSetCanary);
+        // }
+
+        console.log('priority.equal.and.name.equal', false);
+
+        return false;
       }
-    } else if (supersetCanary.priority > subsetCanary.priority) {
-      supersetCanary.style = {
+    } else if (generalSetCanary.priority > preciseSetCanary.priority) {
+      // if(!generalSetCanary.weight){
+      generalSetCanary.style = {
         stroke: '#D8D8D8',
         lineDash: [5, 5],
       };
-      supersetCanary.weight = 'low';
+      generalSetCanary.weight = 'low';
+      generalSetCanary.edgeType = 'mid-point-edge';
+      // generalSetCanary.edgeType = 'custom-edge';
+      lowWeightCanaryMap.set(generalSetCanary.name, generalSetCanary);
+      // }
 
-      subsetCanary.style = {
-        stroke: '#FF5C00',
+      // if(!preciseSetCanary.weight){
+      preciseSetCanary.style = {
+        stroke: 'l(0) 0:#FF0000 1:#FEA9A9',
       };
-      subsetCanary.weight = 'high';
+      preciseSetCanary.weight = 'high';
+      preciseSetCanary.edgeType = 'custom-edge';
+      highWeightCanaryMap.set(preciseSetCanary.name, preciseSetCanary);
+      // }
 
-      highWeightCanaryMap.set(subsetCanary.name, subsetCanary);
-      lowWeightCanaryMap.set(supersetCanary.name, supersetCanary);
+      console.log('priority.more.than', true);
 
       return true;
     }
   }
-
-  supersetCanary.style = {
-    stroke: '#FF5C00',
-  };
-  supersetCanary.weight = 'high';
-
-  subsetCanary.style = {
-    stroke: '#FF5C00',
-  };
-  subsetCanary.weight = 'high';
-
-  highWeightCanaryMap.set(supersetCanary.name, supersetCanary);
-  highWeightCanaryMap.set(subsetCanary.name, subsetCanary);
-
-  return false;
 }
 
 module.exports = isCanaryShadow;
